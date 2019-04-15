8f40a85f841b445cdb58d2f2e54eb0bcde148b06
wutiancheng <975930271@qq.com>
2019-04-15 10:43:55 +0800

    init

---
+++ src/main/java/com/wtc/elasticsearch/java_high_rest/JavaHighRestApplication.java
@@ -0,0 +1,13 @@
+package com.wtc.elasticsearch.java_high_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHighRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaHighRestApplication.class, args);
+    }
+
+}
