3498f4d10f682271ad7a92c8bcd1d6c5395bd050
wutiancheng <975930271@qq.com>
2019-04-17 00:46:47 +0800

    get api(部分)

--- src/test/java/com/wtc/elasticsearch/DocumentAPIs/GetAPI.java
+++ src/test/java/com/wtc/elasticsearch/DocumentAPIs/GetAPI.java
@@ -30,9 +30,16 @@ public class GetAPI extends JavaHighRestApplicationTests {
     public void simpleExample() {
         GetRequest request = new GetRequest(index, type, id);
 
-        // GETT posts/doc/1?_source=false 禁用_source 返回的_source为null  默认是启动
+        // GET posts/doc/1?_source=false 禁用_source 返回的_source为null  默认是启动
         request.fetchSourceContext(FetchSourceContext.DO_NOT_FETCH_SOURCE);
-        log.info("dsl:{}", request);
+        StringBuilder dsl = new StringBuilder();
+        log.info("dsl:\n{}",
+                dsl.append("GET ")
+                    .append(request.index()).append("/")
+                    .append(request.type()).append("/")
+                    .append(request.id()).append("/?")
+                    .append("_source").append(request.fetchSourceContext().fetchSource())
+                    .toString());
 
         GetResponse response = null;
         try {
@@ -55,15 +62,18 @@ public class GetAPI extends JavaHighRestApplicationTests {
     public void OrderSourceField() {
         GetRequest request = new GetRequest(index, type, id);
 
-//        String[] includes = new String[]{"user"};
+//        GET posts/doc/1?_source_includes=user,message
+//        String[] includes = new String[]{"user", "message"};
 //        String[] excludes = Strings.EMPTY_ARRAY;
 
+//        GET posts/doc/1?_source_excludes=use*
         String[] includes = Strings.EMPTY_ARRAY;
-        String[] excludes = new String[]{"user"};
+        String[] excludes = new String[]{"use*"};
 
         FetchSourceContext fetchSourceContext = new FetchSourceContext(true, includes, excludes);
         request.fetchSourceContext(fetchSourceContext);
-        log.info("dsl:{}", request);
+
+        log.info("{}&{}", request.fetchSourceContext().includes(), request.fetchSourceContext().excludes());
 
         GetResponse response = null;
         try {
@@ -73,4 +83,11 @@ public class GetAPI extends JavaHighRestApplicationTests {
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void storedFields() {
+        GetRequest request = new GetRequest(index, type, id);
+        request.storedFields("message");
+    }
+
 }
